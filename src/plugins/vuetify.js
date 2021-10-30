87ac5fef4bce96bb0bde51479dec3b35d3045e97
Vitaly Krivoshei <vitaly.krivoshei@bfb.one>
2021-10-30 20:48:23 +0300

    a. mdi local icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,15 @@
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
-const opts = {}
-
-export default new Vuetify(opts)
+export default new Vuetify({
+  options: {
+    customProperties: true,
+  },
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+})
