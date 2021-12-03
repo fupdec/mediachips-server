dd9b96d1bd373c119af50a0ba4760fa86496b7ea
Vitaly Krivoshei <vitaly.krivoshei@bfb.one>
2021-12-03 16:56:18 +0300

    a. images to meta items

--- src/components/meta/ItemCard.vue
+++ src/components/meta/ItemCard.vue
@@ -1,30 +1,29 @@
 <template>
   <v-lazy>
     <v-card :class="{favorite: isFavorite}" outlined hover class="meta-card">
-      <v-responsive>
-        <v-img :src="imgMain" :aspect-ratio="meta.MetaSetting.imageAspectRatio" position="top"/>
-        <v-rating  
-          :value="item.rating" 
-          class="rating rating-wrapper"
-          color="yellow darken-2" background-color="grey darken-1"
-          empty-icon="mdi-star-outline" half-icon="mdi-star-half-full"
-          dense half-increments hover clearable />
-        
-        <v-btn 
-          @click="isFavorite = !isFavorite" icon absolute 
-          :color="isFavorite===false ? 'white' : 'pink'" class="fav-btn"
-        > <v-icon :color="isFavorite===false?'grey':'pink'">mdi-heart-outline</v-icon>
-        </v-btn>
-      </v-responsive>
-      <!-- <div>ItemID: {{item.id}}</div> -->
-      <div class="pl-1"> {{item.name}}
+      <div class="img-container">
+        <v-icon v-if="meta.MetaSetting.color" class="meta-color" :color="item.color">mdi-circle</v-icon>
+        <!-- <div v-if="meta.MetaSetting.country" class="country"> <div v-for="c in item.country.split(',')" :key="c" class="flag-icon"> {{c}} </div> </div> -->
+        <v-img :src="images.main" :aspect-ratio="meta.MetaSetting.imageAspectRatio" :class="{show:!images.alt}" position="top" class="main-img"/>
+        <v-img v-if="images.alt" :src="images.alt" :aspect-ratio="meta.MetaSetting.imageAspectRatio" position="top" class="secondary-img"/> 
+        <div v-if="images.custom1" class="custom1-img-button">1</div> <v-img :src="images.custom1" class="custom1-img"/>
+        <div v-if="images.custom2" class="custom2-img-button">2</div> <v-img :src="images.custom2" class="custom2-img"/>
+        <v-btn v-if="meta.MetaSetting.favorite" icon absolute :color="item.favorite?'pink':'white'" class="fav-btn"> <v-icon :color="item.favorite?'pink':'grey'">mdi-heart-outline</v-icon> </v-btn>
+        <div v-if="meta.MetaSetting.rating" class="rating-wrapper"> <v-rating :value="item.rating" dense half-increments hover clearable color="yellow darken-2" background-color="grey" empty-icon="mdi-star-outline" half-icon="mdi-star-half-full"/> </div>
+        <v-icon v-if="meta.MetaSetting.bookmark && item.bookmark" class="bookmark" color="red" :title="item.bookmark">mdi-bookmark</v-icon>
+      </div>
+
+      <div class="px-1 name">{{item.name}}</div>
+      <div class="px-1">
         <v-chip outlined>
-          <v-icon class="mr-1">mdi-video-outline</v-icon> {{numberOfMedia}}
+          <v-icon class="mr-1">mdi-video-outline</v-icon> <b>{{numberOfMedia}}</b>
         </v-chip>
         <v-chip outlined>
-          <v-icon class="mr-1">mdi-eye-outline</v-icon> {{item.views}}
+          <v-icon class="mr-1">mdi-eye-outline</v-icon> <b>{{item.views}}</b>
         </v-chip>
-      </div>  
+      </div>
+      <div v-if="meta.MetaSetting.synonyms && item.synonyms" class="px-1 synonyms"> <span class="pl-2"/> {{item.synonyms}} </div>
+      
       <v-icon v-if="item.bookmark" class="bookmark" color="red" :title="item.bookmark">mdi-bookmark</v-icon>
     </v-card>
   </v-lazy>
@@ -43,36 +42,46 @@ export default {
   },
   mounted() {
     this.$nextTick(() => {
-      this.getImg()
+      this.getImages()
       this.countMediaInItem()
     })
   },
   data: () => ({
     numberOfMedia: 0,
-    imgMain: null,
+    images: {
+      main: null,
+      alt: null,
+      custom1: null,
+      custom2: null,
+    },
   }),
   computed: {
     apiUrl() { return this.$store.state.localhost },
     isFavorite() { return this.item.favorite },
   },
   methods: {
-    getImg() { 
-      let url = `/api/get-file`
-      let imgPath = path.join(__dirname, '/userfiles/media/meta/', this.meta.oldId, this.item.oldId + '_main.jpg') 
+    getImages() {
+      const imageTypes = ['main','alt','custom1','custom2']
+      const settings = this.meta.MetaSetting.imageTypes
+      for (let i of imageTypes) {
+        if (settings.includes(i)) this.getImg(i)
+      }
+    },
+    getImg(imgType) { 
       axios({
         method: 'post',
-        url: this.apiUrl + url,
+        url: this.apiUrl + '/api/get-file',
         responseType: 'blob',
         data: {
-          url: imgPath,
+          url: path.join(__dirname, '/userfiles/media/meta/', this.meta.oldId, `${this.item.oldId}_${imgType}.jpg`),
         }
       })
         .then(res => {
-          this.imgMain = URL.createObjectURL(res.data)
+          this.images[imgType] = URL.createObjectURL(res.data)
         })
         .catch(e => {
-          this.imgMain = path.join(__dirname, '/images/ghost.png') 
-          console.log(e)
+          if (imgType=='main') this.images[imgType] = path.join(__dirname, '/images/ghost.png') 
+          // console.log(e)
         })
     },
     countMediaInItem() {
