f40be0e40888159db2180f41816fb30625a73948
Vitaly Krivoshei <vitaly.krivoshei@bfb.one>
2022-01-09 17:55:34 +0300

    improved watched folders

--- src/components/settings/WatchedFolders.vue
+++ src/components/settings/WatchedFolders.vue
@@ -15,101 +15,29 @@
       </v-tooltip>
     </div>
     <v-list dense v-if="folders.length" shaped class="pb-0 pl-2">
-      <v-list-item v-for="(folder, i) in folders" :key="i" class="folder-list">
-        <div class="folder-item">
-          <div v-if="folderNameEdit == i" class="name">
-            <v-btn
-              @click="folderNameEdit = -1"
-              class="mr-1"
-              color="red"
-              icon
-              fab
-              x-small
-              title="Cancel"
-            >
-              <v-icon>mdi-close</v-icon>
-            </v-btn>
-            <v-text-field
-              v-model="folderName"
-              class="d-inline-flex"
-              hide-details
-              outlined
-              dense
-            />
-            <v-btn
-              @click="renameFolder(i)"
-              :disabled="folderName == ''"
-              class="ml-1"
-              color="green"
-              icon
-              fab
-              x-small
-              title="Save folder name"
-            >
-              <v-icon>mdi-check</v-icon>
-            </v-btn>
-          </div>
-          <div v-else class="name">
-            <v-btn
-              @click="(folderNameEdit = i), (folderName = folder.name)"
-              class="mr-1"
-              icon
-              fab
-              x-small
-              title="Edit folder name"
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-            <v-chip label outlined :title="folder.name">
-              <span>{{ folder.name }}</span>
-            </v-chip>
-          </div>
-          <v-chip
-            @click="openFolder(folder.path)"
-            label
-            outlined
-            class="path"
-            :title="`Open in explorer: ${folder.path}`"
-          >
-            <v-icon left class="icon-closed">mdi-folder</v-icon>
-            <v-icon left class="icon-open">mdi-folder-open</v-icon>
-            <span>{{ folder.path }}</span>
-          </v-chip>
-        </div>
-        <v-switch
-          v-model="folder.watch"
-          @change="updateFolders"
-          class="ml-4 mt-0 pt-0"
-          hide-details
-          title="Toggle watch"
-        />
-        <v-btn
-          @click="removeFolder(i)"
-          class="ml-2"
-          color="red"
-          icon
-          fab
-          x-small
-          title="Remove folder"
-        >
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-      </v-list-item>
+      <WatchedFolder
+        v-for="(f, i) in folders"
+        :key="i"
+        :folder="f"
+        @open="open(i)"
+        @rename="rename(i)"
+        @remove="remove(i)"
+        @toggle="toggle(i)"
+      />
     </v-list>
     <div v-else class="text-center overline pt-2">
       <v-icon size="40">mdi-folder-outline</v-icon>
       <div>There are no watched folders yet</div>
     </div>
     <v-card-actions>
-      <v-btn @click="addFolder" rounded dark color="green" class="pr-4">
-        <v-icon left>mdi-plus</v-icon> Add watched folder
+      <v-btn @click="addFolder" rounded depressed color="success" class="pr-4">
+        <v-icon left>mdi-plus</v-icon> Add
       </v-btn>
       <v-spacer></v-spacer>
       <div class="d-flex align-center">
-        <div class="mr-6">Watch folders:</div>
+        <div class="mr-6">Watch:</div>
         <v-switch
           v-model="watchFolders"
-          :label="watchFolders ? 'Yes' : 'No'"
           inset
           class="d-inline"
           :disabled="folders.length == 0"
@@ -125,6 +53,10 @@ import axios from "axios";
 
 export default {
   name: "WatchedFolders",
+  components: {
+    WatchedFolder: () =>
+      import("@/components/settings/folders/WatchedFolder.vue"),
+  },
   mounted() {
     this.$nextTick(async () => {
       await this.getFolders();
@@ -169,7 +101,7 @@ export default {
           console.log(e);
         });
     },
-    async renameFolder(i) {
+    async rename(i) {
       const folder = this.folders[i];
       await axios({
         method: "put",
@@ -187,7 +119,7 @@ export default {
           console.log(e);
         });
     },
-    async removeFolder(i) {
+    async remove(i) {
       const folder = this.folders[i];
       await axios({
         method: "delete",
@@ -203,12 +135,14 @@ export default {
         });
     },
     updateFolders() {},
+    open() {},
+    toggle() {},
   },
 };
 </script>
 
 
-<style lang="scss" scoped>
+<style lang="scss">
 .folder-list {
   display: flex;
   justify-content: space-between;
